4c14318915252f0722c1d3c799431fdb8a0caec2
Liam Fisher <LiamFisher1@gmail.com>
2018-06-20 19:17:58 +0100

    Added custom provider

---
+++ terraform-provider-lfshr/provider.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+			"lfshr_server": resourceServer(),
+		},
+	}
+}
